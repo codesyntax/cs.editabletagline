58ad0c93ddd0e9300e1952446aac2f5c710de93b
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-01-29 12:13:05 +0100

    add specific permission to edit the tagline and provide an upgrade step

---
+++ cs/editabletagline/upgrades.py
@@ -0,0 +1,23 @@
+from Products.CMFCore.utils import getToolByName
+from Persistence import PersistentMapping
+
+import logging
+
+PROFILE_ID = 'profile-cs.editabletagline:default'
+
+
+def upgrade_to_1001(context, logger=None):
+    """Upgrade to cs.editablefooter 2.0"""
+    if logger is None:
+        # Called as upgrade step: define our own logger.
+        logger = logging.getLogger('cs.editabletagline')
+
+    setup = getToolByName(context, 'portal_setup')
+    # Remove previous controlpanel registration
+    controlpanel = getToolByName(context, 'portal_controlpanel')
+    controlpanel.unregisterConfiglet('EditableTagline')
+    # import permission settings
+    setup.runImportStepFromProfile(PROFILE_ID, 'rolemap')
+    # import new controlpanel
+    setup.runImportStepFromProfile(PROFILE_ID, 'controlpanel')
+    logger.info("All set")
